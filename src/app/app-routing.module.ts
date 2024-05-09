4e1dc350c3f28a1f9c25e32006e534d1f53cb9a1
Ulises Britos <ubritos@gmail.com>
2024-05-09 18:19:13 -0300

    Agr registro empleado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegistroComponent } from './registro/registro.component';
+import { RegistroEmpleadoComponent } from './registro-empleado/registro-empleado.component';
 import { VerPerfilComponent } from './ver-perfil/ver-perfil.component';
 import { CambiarContraComponent } from './cambiar-contra/cambiar-contra.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'registrar', component: RegistroComponent},
+  {path: 'registrar empleado', component: RegistroEmpleadoComponent},
   {path: 'ver perfil', component: VerPerfilComponent},
   {path: 'cambiar contra', component: CambiarContraComponent},
   {path: '**', redirectTo: '/home', pathMatch:'full'}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RegistroComponent } from './registro/registro.component';
+import { RegistroEmpleadoComponent } from './registro-empleado/registro-empleado.component';
 import { VerPerfilComponent } from './ver-perfil/ver-perfil.component';
 import { CambiarContraComponent } from './cambiar-contra/cambiar-contra.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     RegistroComponent,
     VerPerfilComponent,
     CambiarContraComponent,
+    RegistroEmpleadoComponent
   ],
   imports: [
     BrowserModule,
