5b8b2a32574dc6fd8c4dfb073b43287535bc4844
PATRICIO BENJAMIN SERRES <patricioserres@gmail.com>
2024-05-12 23:45:43 -0300

    Agrego ver perfil basico

--- src/app/modificar-perfil/modificar-perfil.component.ts
+++ src/app/modificar-perfil/modificar-perfil.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { __values } from 'tslib';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modificar-perfil',
   templateUrl: './modificar-perfil.component.html',
   styleUrls: ['./modificar-perfil.component.css']
 })
-export class ModificarPerfilComponent {
+export class ModificarPerfilComponent  {
+  fecha: Date = new Date(2003, 5, 16);
 
+  userForm = new FormGroup({
+    name: new FormControl('nombre',
+      Validators.required),
+    dni: new FormControl('44823594',
+      Validators.required),
+    email: new FormControl('patricio@gmail.com',[
+      Validators.required,
+      Validators.pattern('.*@.*')]),
+    sucursal: new FormControl('La Plata',
+    Validators.required)
+  });
+
+  private _router = inject(Router)
+
+  constructor(private formBuilder: FormBuilder){
+  }
+
+  hasErrors(controlName: string, errorType: string) {
+    const control = this.userForm.get(controlName);
+    return control && control.hasError(errorType) && (control.dirty || control.touched);
+  }
+
+  onSubmit(): void {
+    if (this.userForm.invalid) {
+      return; // Detener el envío del formulario si hay errores de validación
+    }
+    console.log(this.userForm.value);
+  };
+
+  navigate(ruta: string): void{
+    this._router.navigate([ruta])
+  }
 }
+
+
+
+
+
+
