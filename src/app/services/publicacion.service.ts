bb6ffd6e472a51047b8de25d82d985f90c1b8c22
Franco Agostinelli <francoagostinelli2000@gmail.com>
2024-05-17 20:43:59 -0300

    Cors habilitados

--- src/app/services/publicacion.service.ts
+++ src/app/services/publicacion.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError, catchError } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Pub } from './pub';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class PublicationService {
   private baseUrl = 'http://127.0.0.1:8000/';
   private apiUrl = this.baseUrl + 'publications/';
@@ -24,32 +23,36 @@ export class PublicationService {
   }
 
   public createPublication(pub: Pub): Observable<Pub> {
-    return this.http.post<Pub>(this.apiUrl, pub)
-      }
+    return this.http.post<Pub>(this.apiUrl, pub, { withCredentials: true })
+      .pipe(catchError(this.handleError));
+  }
 
   public getPublication(id: number): Observable<Pub> {
-    return this.http.get<Pub>(this.apiUrl + id + '/');
-      }
+    return this.http.get<Pub>(this.apiUrl + id + '/', { withCredentials: true })
+      .pipe(catchError(this.handleError));
+  }
 
   public getPhotos(id: number): string {
-    return (`${this.apiUrl}${id}/photos/`);
-      }
+    return `${this.apiUrl}${id}/photos/`;
+  }
 
   public getAllCategories(): Observable<any> {
-    return this.http.get<any>(this.baseUrl + 'categories/');
-      }
+    return this.http.get<any>(this.baseUrl + 'categories/', { withCredentials: true })
+      .pipe(catchError(this.handleError));
+  }
 
   public getPublications(): Observable<Pub[]> {
-    return this.http.get<Pub[]>(this.apiUrl);
-      }
+    return this.http.get<Pub[]>(this.apiUrl, { withCredentials: true })
+      .pipe(catchError(this.handleError));
+  }
 
   public updatePublication(id: number, pub: Pub): Observable<Pub> {
-    return this.http.put<Pub>(this.apiUrl + id + '/', pub);
-      }
+    return this.http.put<Pub>(this.apiUrl + id + '/', pub, { withCredentials: true })
+      .pipe(catchError(this.handleError));
+  }
 
   public deletePublication(id: number): Observable<Pub> {
-    return this.http.delete<Pub>(this.apiUrl + id + '/');
-      }
-
+    return this.http.delete<Pub>(this.apiUrl + id + '/', { withCredentials: true })
+      .pipe(catchError(this.handleError));
+  }
 }
-
