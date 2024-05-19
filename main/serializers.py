7dc7f576f81258be1d0f51f7e2324920f4e4efdf
Ulises Britos <ubritos@gmail.com>
2024-05-19 15:41:02 -0300

    password hash working

--- main/serializers.py
+++ main/serializers.py
@@ -11,6 +11,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Hash the password
+        user.save()
+
+        return user
 
 class SucursalSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,24 +29,19 @@ class CustomAuthTokenSerializer(serializers.Serializer):
     password = serializers.CharField()
 
     def validate(self, attrs):
-        username_or_email = attrs.get('username')
+        email = attrs.get('username')
         password = attrs.get('password')
 
-        if username_or_email and password:
-            # Try to authenticate by username or email
-            user = authenticate(username=username_or_email, password=password)
-            if not user:
-                try:
-                    user_instance = User.objects.get(email=username_or_email)
-                    user = authenticate(username=user_instance.username, password=password)
-                except User.DoesNotExist:
-                    raise serializers.ValidationError('Invalid username/email or password.')
+        if email and password:
+            user = User.objects.filter(email=email).first()
+            if user is None:
+                raise serializers.ValidationError('User = none')
 
-            if not user:
-                raise serializers.ValidationError('Invalid username/email or password.')
+            if not user.check_password(password):
+                raise serializers.ValidationError('User check password')
 
             attrs['user'] = user
         else:
-            raise serializers.ValidationError('Must include "username_or_email" and "password".')
+            raise serializers.ValidationError('Else')
 
         return attrs
