cba177af2b7cafa4690e9acef54b2a77af72f9e9
PATRICIO BENJAMIN SERRES <patricioserres@gmail.com>
2024-05-19 21:57:32 -0300

    cambiar contraseña

--- src/app/cambiar-contra/cambiar-contra.component.ts
+++ src/app/cambiar-contra/cambiar-contra.component.ts
@@ -13,8 +13,8 @@ export class CambiarContraComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) { 
     this.cambiarContraForm = this.formBuilder.group({
-      nuevaContrasena: ['', [Validators.required, Validators.minLength(6), Validators.pattern(".*[!@#$%^&*()_+}{:;'?/><,.\|~`]")]],
-      repetirContrasena: ['', [Validators.required, Validators.minLength(6),  Validators.pattern(".*[!@#$%^&*()_+}{:;'?/><,.\|~`]")]]
+      nuevaContrasena: ['', [Validators.required, Validators.minLength(6), Validators.pattern(".*[!@#$%^&*()_+}{:;'?/><,.\|~`].*")]],
+      repetirContrasena: ['', [Validators.required, Validators.minLength(6),  Validators.pattern(".*[!@#$%^&*()_+}{:;'?/><,.\|~`].*")]]
     });
   }
 
@@ -24,14 +24,22 @@ export class CambiarContraComponent implements OnInit {
 
   hasErrors(controlName: string, errorType: string) {
     const control = this.cambiarContraForm.get(controlName);
-    return control && control.hasError(errorType);
+    return control && control.hasError(errorType) && (control.dirty || control.touched);  
   }
 
-  enviar() {
+  onSubmit() : void{
     this.submitted = true; // Marcar el formulario como enviado al hacer clic en el botón "Enviar"
+
     if (this.cambiarContraForm.invalid) {
+      console.log('El formulario es inválido. No se puede modificar.');
       return; // Detener el envío del formulario si hay errores de validación
     }
+    if (this.cambiarContraForm.get('nuevaContrasena')?.value !== this.cambiarContraForm.get('repetirContrasena')?.value) {
+      console.log('Las contraseñas no son iguales. No se puede modificar.');
+      return; // Detener el envío del formulario si las contraseñas no son iguales
+    }
+
+    console.log('El formulario es válido. Realizando cambio de contraseña...');
     console.log(this.cambiarContraForm.value);
     
     // Aquí puedes realizar la lógica para enviar el formulario al backend
