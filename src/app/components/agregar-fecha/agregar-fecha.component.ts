5fe53e9cf27735808178bf277d396b538f957f0f
Franco Agostinelli <francoagostinelli2000@gmail.com>
2024-06-05 18:46:46 -0300

    agregar-fecha-sin test pq no me dejan

--- src/app/components/agregar-fecha/agregar-fecha.component.ts
+++ src/app/components/agregar-fecha/agregar-fecha.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SucursalService } from 'src/app/services/sucursal.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-fecha',
@@ -7,11 +9,31 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./agregar-fecha.component.css']
 })
 export class AgregarFechaComponent implements OnInit {
-  tradeID?: number;
+  tradeID: number;
+  agregarFechaForm: FormGroup;
+  sucursales: string[] =[];
+  minDate: string;
 
-  constructor(private route: ActivatedRoute){}
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private sucursalService: SucursalService){
+    this.agregarFechaForm = this.fb.group({
+      sucursal: ['', Validators.required],
+      fecha: ['', Validators.required] //(YYYY-MM-DD) 
+    });
     this.tradeID = parseInt(this.route.snapshot.params['id'], 10);
+    sucursalService.getSucursales().subscribe((sucursal) => {
+      this.sucursales=(sucursal.map(item => item.direccion));
+    }
+  )
+  const today = new Date();
+  this.minDate = today.toISOString().split('T')[0]; // Formato 'yyyy-MM-dd'
+}
+  ngOnInit(): void {
+    
   }
+
+  guardarTrueque(){
+    console.log(this.agregarFechaForm.value);
+
+  }
+
 }
