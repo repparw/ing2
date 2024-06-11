91887b3befd1a41252147cb4243486ee94f0e9cf
Ulises Britos <ubritos@gmail.com>
2024-06-11 11:51:30 -0300

    feat(ver-sucursal): stats only for admin, emplSuc

--- src/app/components/ver-sucursal/ver-sucursal.component.ts
+++ src/app/components/ver-sucursal/ver-sucursal.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { forkJoin, of } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
+import { forkJoin, Observable, of } from 'rxjs';
+import { catchError, switchMap, map } from 'rxjs/operators';
 import { SucursalService } from '../../services/sucursal.service';
 import { StatisticsService } from '../../services/statistics.service';
 import { TradeService } from '../../services/trade.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-ver-sucursal',
@@ -15,29 +16,38 @@ export class VerSucursalComponent implements OnInit {
   sucursal: any;
   statistics: any;
   noStatistics: boolean = false;
+  isAdmin$: Observable<boolean>;
+  isEmployee$: Observable<boolean>;
+  sucEmpl?: string;
 
   constructor(
     private route: ActivatedRoute,
     private sucursalService: SucursalService,
     private statisticsService: StatisticsService,
-    private tradeService: TradeService
-  ) { }
+    private tradeService: TradeService,
+    private userService: UserService
+  ) {
+    this.isAdmin$ = this.userService.isAdmin$;
+    this.isEmployee$ = this.userService.isEmployee$;
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
 
-    // First fetch the sucursal details
-    this.sucursalService.getSucursal(id as unknown as number).pipe(
+    this.userService.getCurrentUser().pipe(
+      switchMap(user => {
+        this.sucEmpl = user.suc as unknown as string;
+        return this.sucursalService.getSucursal(id as unknown as number);
+      }),
       switchMap(sucursal => {
         this.sucursal = sucursal;
 
-        // Then fetch trade proposals by sucursal
         return this.tradeService.getTradeProposalsBySucursal(sucursal.id as unknown as number);
       }),
       switchMap(tradeProposals => {
         if (tradeProposals.length === 0) {
           this.noStatistics = true; // No trade proposals, no need to fetch statistics
-          return [];
+          return of([]);
         } else {
           // Fetch statistics only if there are trade proposals
           return forkJoin({
@@ -49,10 +59,10 @@ export class VerSucursalComponent implements OnInit {
       catchError(error => {
         console.error('Error fetching data:', error);
         this.noStatistics = true;
-        return [];
+        return of([]);
       })
     ).subscribe(
-      ({ statistics, tradeProposals }) => {
+      ({ statistics, tradeProposals }: any) => {
         this.statistics = statistics;
       },
       error => {
